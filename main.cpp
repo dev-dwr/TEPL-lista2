364e78108aa890ebd4087c3682ee38a159bf4a7d
dev-dwr <dawid.jscript@gmail.com>
2022-10-23 14:52:41 +0200

    changes for concatenating two arrays with + operator

--- main.cpp
+++ main.cpp
@@ -32,12 +32,17 @@ public:
         s_name = pcOther.s_name + "_copy";
         tableSize = pcOther.tableSize;
         std::cout << "copy: " + s_name << std::endl;
-        array = pcOther.array;
+        array= new int[tableSize];
+        for (int i = 0; i < pcOther.tableSize; i++) {
+            array[i] = pcOther.array[i];
+        }
     }
-
 //    ~CTable() {
+//        delete array;
+//        tableSize = 0;
 //        std::cout << "usuwam: " + s_name << std::endl;
 //    }
+
 
     void setName(std::string sName) {
         if (sName.empty()) {
@@ -52,23 +57,35 @@ public:
             std::cout << "we did not changed array size, reason: negative num" << std::endl;
             return false;
         }
-        array = new int[iTableLen];
         tableSize = iTableLen;
-        std::cout << "we changed array size bSetNewSize: " << iTableLen << std::endl;
+        int *newArr = new int[tableSize];
+        if(tableSize <= iTableLen){
+            for (int i = 0; i < tableSize; i++) {
+                newArr[i] = array[i];
+            }
+        }else{
+            for (int i = 0; i < iTableLen; i++) {
+                newArr[i] = array[i];
+            }
+        }
+        std::cout << "we changed array size bSetNewSize, new size: " << iTableLen << std::endl;
         return true;
     }
 
     CTable *pcClone() {
-        CTable *clone;
-        clone = new CTable(s_name, tableSize);
-        return clone;
+//        CTable *clone;
+//        clone = new CTable(s_name, tableSize);
+        return new CTable(*this);
     }
 
 
     void operator=(const CTable &pcOther) {
+//        for (int i = 0; i < pcOther.tableSize; i++) {
+//            array[i] = pcOther.array[i];
+//        }
         array = pcOther.array;
         tableSize = pcOther.tableSize;
-        std::cout << "we changed array size : " << tableSize << std::endl;
+        std::cout << "we changed array size to: " << tableSize << std::endl;
     }
 
     void vSetValueAt(int iOffset, int iNewVal) {
@@ -81,9 +98,9 @@ public:
         }
     }
 
-    CTable operator+(const CTable &pcOther1) {
+    int* operator+(const CTable &pcOther1) {
         int newSize = pcOther1.tableSize + tableSize;
-        int newArr[newSize];
+        int* newArr = new int[newSize];
         for(int i = 0; i< newSize; i++){
             if(i < tableSize){
                 newArr[i] = array[i];
@@ -91,8 +108,8 @@ public:
                 newArr[i] = pcOther1.array[i-tableSize];
             }
         }
-        array = newArr;
-        return CTable();
+//        array = newArr;
+        return newArr;
     }
 
 };
@@ -104,6 +121,10 @@ int main() {
     c_tab_1.bSetNewSize(4);
     // z deknstruktorem program zaincjalizowal obiekty zmienil size tablicy i usunal obiekty  // bez deknstruktora program nie usunal obiektow po zakonsczeniu
     //initialize c_tab_1 table
+
+    c_tab_0 = c_tab_1;
+    std::cout << "\n" << std::endl;
+
     c_tab_1.vSetValueAt(0, 51);
     c_tab_1.vSetValueAt(1, 52);
     c_tab_1.vSetValueAt(2, 53);
@@ -116,15 +137,18 @@ int main() {
     c_tab_0.vSetValueAt(4, 5);
     c_tab_0.vSetValueAt(5, 6);
 
-    c_tab_0 = c_tab_1;
     c_tab_1.vSetValueAt(2, 123);
-    c_tab_0.vPrint();
+//
+//    c_tab_0.vPrint();
+//    std::cout << "\n" << std::endl;
+//    c_tab_1.vPrint();
+
     std::cout << "\n" << std::endl;
-    c_tab_1.vPrint();
+    int* arr;
+    arr = c_tab_0+c_tab_1;
 
-    CTable c_tab_res;
-    c_tab_0+c_tab_1;
-
-
+    for(int i = 0; i < sizeof(arr); i++){
+        std::cout << arr[i] << std::endl;
+    }
     return 0;
 }
